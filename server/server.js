d8b44be197678cf73627bb22ef53d3a267821771
bcs1993 <you@example.com>
2023-04-20 22:37:34 -0500

    Commit message

---
+++ server/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { config } from 'dotenv';
+import router from './router/route.js';
+
+
+/** import connection file */
+import connect from './database/conn.js';
+
+const app = express();
+
+
+/** Middlewares */
+app.use(morgan('tiny'))
+app.use(cors());
+app.use(express.json());
+config();
+
+
+/** App Port */
+const port = process.env.PORT || 8080;
+
+
+
+
+/** Routes */
+app.use('/api', router); /** apis */
+
+
+app.get('/', (req, res) => {
+    try {
+        res.json("Get Request");
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+
+/** start server only when we have valid connection */
+connect().then (() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to server")
+    }
+
+}).catch(error => {
+    console.log("Invalid Database Connection");
+})
+
+
